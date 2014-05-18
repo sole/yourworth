b7fcc9200f0316398e3adb9a71f2236f70618550
sole <sole@mozilla.com>
2014-05-18 19:37:11 +0100

    showing some data, but only the first page of results and without fancy animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 (function() {
 
+	var reposCount = document.querySelector('#numberOfRepositories span');
+	var forkedReposCount = document.querySelector('#numberOfForkedRepositories span');
+	var favouritesCount = document.querySelector('#numberOfFavourites span');
+	var worthCount = document.querySelector('#totalWorth span');
 	var form = document.querySelector('form');
 	var usernameInput = document.getElementById('username');
+
 	form.addEventListener('submit', onFormSubmit, false);
 
 	function onFormSubmit(e) {
@@ -15,13 +20,52 @@
 	}
 
 	function onInfoLoaded(error, data) {
+
 		console.log('got the info, or not');
 
 		if(error) {
 			console.error('nope', error);
 		} else {
-			console.log(data);
+			var info = extractInfo(data);
+			displayResults(info);
 		}
+
+	}
+
+	function extractInfo(reposData) {
+		var numberOfRepos = 0;
+		var numberOfForkedRepos = 0;
+		var numberOfFavourites = 0;
+
+		reposData.forEach(function(repo) {
+			console.log(repo.name, repo.fork, repo.stargazers_count);
+			if(repo.fork) {
+				numberOfForkedRepos++;
+			} else {
+				numberOfRepos++;
+			}
+
+			numberOfFavourites += repo.stargazers_count;
+
+		});
+
+		return ({
+			numberOfRepos: numberOfRepos,
+			numberOfForkedRepos: numberOfForkedRepos,
+			numberOfFavourites: numberOfFavourites
+		});
+	}
+
+	function displayResults(info) {
+		var repos = info.numberOfRepos;
+		var forks = info.numberOfForkedRepos;
+		var favourites = info.numberOfFavourites;
+		var totalWorth = magicFormula(repos, forks, favourites);
+
+		reposCount.innerHTML = repos;
+		forkedReposCount.innerHTML = forks;
+		favouritesCount.innerHTML = favourites;
+		worthCount.innerHTML = totalWorth;
 
 	}
 

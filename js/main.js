919d212ab6fccc2a4aacabcd42e2884245f4e18d
sole <sole@mozilla.com>
2014-05-18 10:59:23 +0100

    some markup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('hey');
+function requestUserInfo(userName, doneCallback) {
+	var apiURL = 'https://api.github.com/users/' + userName + '/repos';
+	var request = new XMLHttpRequest();
+
+	request.open('get', url, true);
+    request.responseType = 'json';
+
+	request.onerror = function(e) {
+		doneCallback('Sad times, cannot get the info');
+	};
+
+	request.onload = function() {
+		doneCallback(false, request.response);
+	};
+}

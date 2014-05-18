d309806a3fbe49d3fee57ec992d8454fc92b52c3
sole <sole@mozilla.com>
2014-05-18 11:07:40 +0100

    yay for properly cors-configured servers!

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
-function requestUserInfo(userName, doneCallback) {
-	var apiURL = 'https://api.github.com/users/' + userName + '/repos';
-	var request = new XMLHttpRequest();
+(function() {
 
-	request.open('get', url, true);
-    request.responseType = 'json';
+	var form = document.querySelector('form');
+	var usernameInput = document.getElementById('username');
+	form.addEventListener('submit', onFormSubmit, false);
 
-	request.onerror = function(e) {
-		doneCallback('Sad times, cannot get the info');
-	};
+	function onFormSubmit(e) {
+		e.preventDefault();
 
-	request.onload = function() {
-		doneCallback(false, request.response);
-	};
-}
+		var username = usernameInput.value;
+		
+		if(username !== '') {
+			requestUserInfo(username, onInfoLoaded);
+		}
+	}
+
+	function onInfoLoaded(error, data) {
+		console.log('got the info, or not');
+
+		if(error) {
+			console.error('nope', error);
+		} else {
+			console.log(data);
+		}
+
+	}
+
+	function requestUserInfo(userName, doneCallback) {
+
+		var apiURL = 'https://api.github.com/users/' + userName + '/repos';
+		var request = new XMLHttpRequest();
+
+		request.open('get', apiURL, true);
+		request.responseType = 'json';
+
+		request.onerror = function(e) {
+			doneCallback('Sad times, cannot get the info');
+		};
+
+		request.onload = function() {
+			doneCallback(false, request.response);
+		};
+
+		request.send();
+	}
+
+}).call(this);
